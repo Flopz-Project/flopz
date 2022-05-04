efeca38f7f91f582057d636b1b10605616fd845e
Luca Dubies <luca@noelscher.com>
2022-05-04 09:49:06 +0200

    updatin setup.py and flopz/__init__ for potential PyPI deployment

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
-
 from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name='flopz',
-    version='1.0',
+    version='0.1.1',
     packages=find_packages(),
-    url='',
-    license='',
+    url='https://github.com/Flopz-Project/flopz',
+    license='Apache License',
     author='Noelscher Consulting GmbH',
     author_email='ferdinand@noelscher.com',
-    description='flopz - Low Level Assembler and Firmware Instrumentation Toolkit'
+    description='flopz - Low Level Assembler and Firmware Instrumentation Toolkit',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=['bitstring'],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.7',
+    ],
 )
