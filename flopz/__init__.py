eb470811e4a8be5e5d625209b0f8eb7ccd1d5da3
Luca Dubies <luca@noelscher.com>
2022-05-18 11:29:03 +0200

    updating version to 0.2.0

--- flopz/__init__.py
+++ flopz/__init__.py
@@ -4,5 +4,5 @@ flopz.
 Low Level Assembler and Firmware Instrumentation Toolkit
 """
 
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 __author__ = "Noelscher Consulting GmbH"
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='flopz',
-    version='0.1.1',
+    version='0.2.0',
     packages=find_packages(),
     url='https://github.com/Flopz-Project/flopz',
     license='Apache License',
